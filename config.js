ff7f8f15a975bb4b55ade59c963bc7bfa9c2908c
Ilhamfendd <ismaililham066@gmail.com>
2026-06-25 23:21:26 +0700

    ganti model lagi (deepseek v3.2)

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 module.exports = {
+
   BOT_NAME: "!ai",
-  AI_MODEL: "DeepSeek-V3", // atau "DeepSeek-R1"
-  API_KEY: process.env.API_KEY,
-  API_BASE_URL: "https://api.sambanova.ai/v1"
+
+  AI_MODEL:
+    "DeepSeek-V3.2",
+
+  API_KEY:
+    process.env.API_KEY,
+
+  API_BASE_URL:
+    "https://api.sambanova.ai/v1"
+
 }
